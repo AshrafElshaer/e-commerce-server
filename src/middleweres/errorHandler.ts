b2127e739d24e6143617140427592308061eb0c9
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2022-11-08 01:31:26 -0600

    setup basic express serverwith logevents and error handler

---
+++ src/middleweres/errorHandler.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+import { logEvents } from "./logEvents";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  logEvents(`${err.name}: ${err.message}`, "errLog.txt");
+  console.error(err.stack);
+  res.status(500).send(err.message);
+};
---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import cors from "cors";
+import { corsOptions } from "./config/corsOptions";
+import { errorHandler } from "./middleweres/errorHandler";
+import { logger } from "./middleweres/logEvents";
+const app = express();
+const PORT: string = process.env.PORT || "8080";
+
+
+// loging incoming requests
+app.use(logger);
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+//middleware for cookies
+// app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  console.log(req.headers.origin);
+  res.send("hello from server");
+});
+
+
+// error handler
+app.use(errorHandler);
+app.listen(PORT, () => console.log(`server is listening on Port : ${PORT}`));
