dbad649e904a8c0f6004cafdd9c8f3598051d5b2
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2022-12-26 23:58:32 -0600

    add logic to login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import bcrypt from "bcrypt";
 import JWT from "jsonwebtoken";
 import UserModel from "../models/users.schema";
-import { type } from "os";
 dotenv.config();
 
 export type TNewUser = {
@@ -14,8 +13,59 @@ export type TNewUser = {
 };
 // POST /loging
 
-export const loginUser = (req: Request, res: Response) => {
-  res.send("login user");
+export const loginUser = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  try {
+    const foundUser = await UserModel.findOne({ email });
+    if (!foundUser)
+      return res.status(400).json({
+        message: `${email} doesn't exist please sign up`,
+      });
+
+    const matchPassword = bcrypt.compare(password, foundUser.password);
+
+    if (!matchPassword)
+      return res.status(400).json({
+        message: `Incorrect password`,
+      });
+
+    const accessToken = JWT.sign(
+      {
+        email: foundUser.email,
+        role: foundUser.role,
+      },
+      process.env.ACCESS_TOKEN_SECRET as string,
+      // process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = JWT.sign(
+      {
+        email: foundUser.email,
+        role: foundUser.role,
+      },
+      process.env.REFRESH_TOKEN_SECRET as string,
+      { expiresIn: "1d" }
+    );
+
+    foundUser.refreshToken = refreshToken;
+    await foundUser.save();
+
+    res.cookie("jwt", refreshToken, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    res.json({
+      ...foundUser.toObject(),
+      accessToken,
+      refreshToken: undefined,
+      password: undefined,
+    });
+  } catch (error: any) {
+    res.json({ message: error.message });
+  }
 };
 
 // POST /register
