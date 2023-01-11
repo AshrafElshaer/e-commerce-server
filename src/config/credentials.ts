813bfce1c0b0366bb0c8c9c3fde5a5aa012fb270
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-01-11 01:37:06 -0600

    cors origin

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,13 +1,16 @@
 import { Request, Response, NextFunction } from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const credentials = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  
-{/* @ts-ignore*/}
+  /* @ts-ignore*/
+
   res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL);
 
   next();
 };
