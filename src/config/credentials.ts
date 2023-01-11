152f91763b33f1dc94686b93e5d33ee1e239b6b2
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2023-01-11 00:03:53 -0600

    c

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -1,14 +1,13 @@
 import { Request, Response, NextFunction } from "express";
-import { allowedOrigins } from "./allowedOrgins";
 
 export const credentials = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin as string)) {
-    res.header("Access-Control-Allow-Credentials", true);
-  }
+  
+{/* @ts-ignore*/}
+  res.header("Access-Control-Allow-Credentials", true);
+
   next();
 };
